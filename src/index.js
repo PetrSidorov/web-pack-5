e7f94675ac01c51b2b6228116e402627819e088a
Peter <piterpatrikk@gmail.com>
2021-09-01 23:02:27 +0300

    babel support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const elevenShieldRecipe = {
     leatherStrips: 2,
     ironIngot: 1,
-
+    refinedMoonstone: 4
 }
+
+const elevenGauntletRecipe = {
+    ...elevenShieldRecipe,
+    leather: 1,
+    refinedMoonstone: 4,
+}
+
+console.log(elevenShieldRecipe);
+console.log(elevenGauntletRecipe);
